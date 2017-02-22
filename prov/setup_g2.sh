71a7971a788cc6863743cf70a43c5d82b1522f86
Shu Sugimoto <shu@su.gimo.to>
2017-02-22 14:06:06 +0900

    Update quagga to 1.1.1

--- prov/setup_g2.sh
+++ prov/setup_g2.sh
@@ -1,17 +1,19 @@
-apt-get install quagga
+# use stretch repository to use quagga 1.1.1
+echo 'deb http://httpredir.debian.org/debian stretch main' >> /etc/apt/sources.list
+echo 'APT::Default-Release "jessie";' > /etc/apt/apt.conf.d/10default-release
+apt-get update
 
-sed -i -e 's/zebra=no/zebra=yes/' /etc/quagga/daemons
-sed -i -e 's/ospfd=no/ospfd=yes/' /etc/quagga/daemons
+# install quagga
+apt-get -y install quagga-core/stretch quagga-ospfd/stretch libncursesw5/stretch libncurses5/stretch libtinfo5/stretch
 
 cp /vagrant/configs/g2/zebra.conf /etc/quagga
 cp /vagrant/configs/g2/ospfd.conf /etc/quagga
-chown quagga: /etc/quagga/zebra.conf
-chown quagga: /etc/quagga/ospfd.conf
-chmod 640 /etc/quagga/zebra.conf
-chmod 640 /etc/quagga/ospfd.conf
+touch /etc/quagga/vtysh.conf
 
-systemctl enable quagga
-systemctl restart quagga
+systemctl enable zebra
+systemctl enable ospfd
+systemctl restart zebra
+systemctl restart ospfd
 
 echo "1" > /proc/sys/net/ipv4/ip_forward
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
